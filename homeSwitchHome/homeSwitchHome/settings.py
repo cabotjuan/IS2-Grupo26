03048a188a6c1088378d8760633c729bd3ff2177
Cabot, Juan Andres <j_cabot@live.com.ar>
2019-05-13 11:57:35 -0300

    probando templates

--- homeSwitchHome/homeSwitchHome/settings.py
+++ homeSwitchHome/homeSwitchHome/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'homeSwitchHome.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['HomeSwitchHome/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
